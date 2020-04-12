e7956bd98154db14b78369468ab7a2cfd8acda9e
tda78 <tda78@mail.ru>
2020-04-12 22:43:33 +0300

    2.1.4

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    @Override
+    public String toString(){
+        return " - Ему кранты";
+    }
+
+}
